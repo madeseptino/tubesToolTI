79044b4ebb95529fc5fcd5101b68645359bfc085
SeptiNo <129021852+madeseptino@users.noreply.github.com>
2023-12-07 23:31:50 +0800

    Create index.php
    dashboard for tubestool

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <!--<style>
+        .mainNavbar{
+            background-color: black;
+        }
+        .brand{
+            width: 180px;
+            position: relative;
+        }
+        .contactUs{
+            color: white;
+            position: absolute;
+            right: 30px;
+            background-color: #;
+        }
+    </style>-->
+</head>
+<body>               
+    <nav class="navbar nbg-body-tertiary mainNavbar">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="Tubes.png" class="brand" alt="">
+                <a href="#" class="contactUs">Contact Us</a>
+            </a>
+        </div>
+    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <section class="jumbotron text-center mt-5">
+        <img src="pp.png" alt="" width="100px" class=" rounded-circle">
+        <h5 class="mt-3">Your Name</h5>
+    </section>
+    <div class="container text-center mainContainer">
+        <div class="row">
+            <div class="card customer1" style="width: 10rem">
+                COLUMN 1
+            </div>
+            <div class="card customer2" style="width: 10rem">
+                COLUMN 2
+            </div>
+            <div class="card" style="width: 10rem">
+                COLUMN 3
+            </div>
+        </div>
+    </div>
+</body>
+</html>
