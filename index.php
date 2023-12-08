14664aa39addc24a580cd3c01fcdb09ee73cebf1
gusaditt <146801690+gusaditt@users.noreply.github.com>
2023-12-08 11:48:36 +0800

    jadiin kkolom index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <!--<style>
+    <style>
         .mainNavbar{
             background-color: black;
         }
@@ -20,7 +20,16 @@
             right: 30px;
             background-color: #;
         }
-    </style>-->
+        .box{
+            padding : 30px;
+            background : #000;
+            margin : 10px 0px;
+            text-align : center;
+        }
+        .no-decoration{
+            text-decoration : none;
+        }
+    </style>
 </head>
 <body>               
     <nav class="navbar nbg-body-tertiary mainNavbar">
@@ -31,23 +40,35 @@
             </a>
         </div>
     </nav>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <section class="jumbotron text-center mt-5">
         <img src="pp.png" alt="" width="100px" class=" rounded-circle">
         <h5 class="mt-3">Your Name</h5>
     </section>
-    <div class="container text-center mainContainer">
+    <div class="container mt-5">
         <div class="row">
-            <div class="card customer1" style="width: 10rem">
-                COLUMN 1
-            </div>
-            <div class="card customer2" style="width: 10rem">
-                COLUMN 2
-            </div>
-            <div class="card" style="width: 10rem">
-                COLUMN 3
-            </div>
+        <div class="col-md-6 col-lg-3 col-xl-4">
+            <div class="box text-light"><a href="" class="no-decoration text-light">CUSTOMER</a></div>
+        </div>
+        <div class="col-md-6 col-lg-3 col-xl-4">
+            <div class="box text-light"><a href="" class="no-decoration text-light">PEGAWAI</a></div> 
+        </div>
+        <div class="col-md-6 col-lg-3 col-xl-4">
+            <div class="box text-light"><a href="" class="no-decoration text-light">BARANG</a></div>
+        </div>
+        <div class="col-md-6 col-lg-3 col-xl-4">
+            <div class="box text-light"><a href="" class="no-decoration text-light">TRANSAKSI</a></div>
+        </div>
+        <div class="col-md-6 col-lg-3 col-xl-4">
+            <div class="box text-light"><a href="" class="no-decoration text-light">CUSTOMER</a></div>
+        </div>
+        <div class="col-md-6 col-lg-3 col-xl-4">
+            <div class="box text-light"><a href="" class="no-decoration text-light">CUSTOMER</a></div>
         </div>
     </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 </html>
