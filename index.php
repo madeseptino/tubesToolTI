4a609aaa162e9acb8a6c672dab27bf0b59b2cfa8
gusaditt <146801690+gusaditt@users.noreply.github.com>
2023-12-10 07:25:26 +0800

    kolom baru + footer.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
         <div class="container">
             <a class="navbar-brand" href="#">
                 <img src="Tubes.png" class="brand" alt="">
-                <a href="#" class="contactUs">Contact Us</a>
+                <a href="#" class="no-decoration contactUs">Contact Us</a>
             </a>
         </div>
     </nav>
@@ -44,31 +44,77 @@
         <img src="pp.png" alt="" width="100px" class=" rounded-circle">
         <h5 class="mt-3">Your Name</h5>
     </section>
-    <div class="container mt-5">
-        <div class="row">
-        <div class="col-md-6 col-lg-3 col-xl-4">
-            <div class="box text-light"><a href="" class="no-decoration text-light">CUSTOMER</a></div>
-        </div>
-        <div class="col-md-6 col-lg-3 col-xl-4">
-            <div class="box text-light"><a href="" class="no-decoration text-light">PEGAWAI</a></div> 
-        </div>
-        <div class="col-md-6 col-lg-3 col-xl-4">
-            <div class="box text-light"><a href="" class="no-decoration text-light">BARANG</a></div>
-        </div>
-        <div class="col-md-6 col-lg-3 col-xl-4">
-            <div class="box text-light"><a href="" class="no-decoration text-light">TRANSAKSI</a></div>
-        </div>
-        <div class="col-md-6 col-lg-3 col-xl-4">
-            <div class="box text-light"><a href="" class="no-decoration text-light">CUSTOMER</a></div>
-        </div>
-        <div class="col-md-6 col-lg-3 col-xl-4">
-            <div class="box text-light"><a href="" class="no-decoration text-light">CUSTOMER</a></div>
-        </div>
-    </div>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+<section class="p-5">
+    <div class="container mt-5">
+        <div class="row text-center g-4">
+                <div class="col-md-6 col-lg-3 col-xl-4">
+                    <div class="card bg-danger text-light">
+                        <div class="card-body text-centre">
+                            <img src="cst.png" width="50px">
+                            <h4 class="card-tittle mb-2">Customer</h4>
+                            <a href="" class="no-decoration btn btn-light">ADD</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 col-xl-4">
+                    <div class="card bg-warning text-light">
+                        <div class="card-body text-centre">
+                            <img src="pgw.png" width="50px">
+                            <h4 class="card-tittle mb-2">Pegawai</h4>
+                            <a href="" class="no-decoration btn btn-light">ADD</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 col-xl-4">
+                    <div class="card bg-success text-light">
+                        <div class="card-body text-centre">
+                            <img src="brg.png" width="50px">
+                            <h4 class="card-tittle mb-2">Barang</h4>
+                            <a href="" class="no-decoration btn btn-light">ADD</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 col-xl-6">
+                    <div class="card bg-primary text-light">
+                        <div class="card-body text-centre">
+                            <img src="trk.png" width="50px">
+                            <h4 class="card-tittle mb-2">Transaksi</h4>
+                            <a href="" class="no-decoration btn btn-light">ADD</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 col-xl-6">
+                    <div class="card bg-secondary text-light">
+                        <div class="card-body text-centre">
+                            <img src="kmb.png" width="50px">
+                            <h4 class="card-tittle mb-2">Pengembalian</h4>
+                            <a href="" class="no-decoration btn btn-light">ADD</a>
+                        </div>
+                    </div>
+                </div>             
+        </div>
+    </div>
+</section>
+
+<footer class="bg-dark text-white text-center py-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h5>Alamat</h5>
+                    <p>Jimbaran</p>
+                </div>
+                <div class="col-md-6">
+                    <h5>Kontak</h5>
+                    <p>Email: sewatenda@gmail.com</p>
+                    <p>No. Telp: +62 123456789</p>
+                </div>
+            </div>
+        </div>
+        <p class="mt-3">&copy; 2023 Sewa Tenda. All Rights Reserved.</p>
+    </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 </html>
